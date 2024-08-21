43660cf4d6d6f318df7cec19efde3d042fff21a0
coby cockrell <116532258+CobyEC@users.noreply.github.com>
2024-08-21 10:22:16 -0400

    Added/Tested Activation Functionality

---
+++ activate.cpp
@@ -0,0 +1,46 @@
+// activation.cpp
+// Author: Coby Cockrell
+// Date: 5/7/2024
+// Purpose: This file implements various activation functions for the neural network.
+
+#include "activate.h"
+#include <algorithm>
+
+float activate::relu(float x) {
+    return std::max(0.0f, x);
+}
+
+float activate::leaky_relu(float x, float alpha) {
+    return x > 0 ? x : alpha * x;
+}
+
+float activate::sigmoid(float x) {
+    return 1.0f / (1.0f + std::exp(-x));
+}
+
+float activate::tanh(float x) {
+    return std::tanh(x);
+}
+
+void activate::softmax(float* input, float* output, int size) {
+    float max_val = input[0];
+    float sum = 0.0f;
+
+    //Find maximum value for numerical stability
+    for (int i = 1; i < size; ++i) {
+        if (input[i] > max_val) {
+            max_val = input[i];
+        }
+    }
+
+    //Compute exponentials and sum
+    for (int i = 0; i < size; ++i) {
+        output[i] = std::exp(input[i] - max_val);
+        sum += output[i];
+    }
+
+    //Normalize
+    for (int i = 0; i < size; ++i) {
+        output[i] /= sum;
+    }
+}
